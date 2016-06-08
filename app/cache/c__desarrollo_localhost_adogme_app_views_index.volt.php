ee18389355c7f1234e8eaca37b4117b3411bf01c
Kyoshi Sakuda <Kyoshi Sakuda>
2016-06-08 11:35:12 -0500

    + .gitignore, cambio en la referencia de estilos y scripts

--- app/cache/c__desarrollo_localhost_adogme_app_views_index.volt.php
+++ app/cache/c__desarrollo_localhost_adogme_app_views_index.volt.php
@@ -5,14 +5,15 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <?php echo $this->tag->getTitle(); ?>
-        <?php echo $this->tag->stylesheetLink('css/bootstrap.min.css'); ?>
-        <?php echo $this->tag->stylesheetLink('css/jquery-ui.structure.min.css'); ?>
-        <?php echo $this->tag->stylesheetLink('css/jquery-ui.theme.min.css'); ?>
-        <?php echo $this->tag->stylesheetLink('css/estilos.css'); ?>
+        
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/jquery-ui.structure.min.css">
+        <link rel="stylesheet" href="css/jquery-ui.theme.min.css">
+        <link rel="stylesheet" href="css/estilos.css">
 
-        <?php echo $this->tag->javascriptInclude('js/jquery.min.js'); ?>
-        <?php echo $this->tag->javascriptInclude('js/bootstrap.min.js'); ?>
-        <?php echo $this->tag->javascriptInclude('js/jquery-ui.min.js'); ?>
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery-ui.min.js"></script>
         <meta name="author" content="kyo">
     </head>
     <body>
--- app/views/index.volt
+++ app/views/index.volt
@@ -5,6 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         {{ get_title() }}
+        {#
         {{ stylesheet_link('css/bootstrap.min.css') }}
         {{ stylesheet_link('css/jquery-ui.structure.min.css') }}
         {{ stylesheet_link('css/jquery-ui.theme.min.css') }}
@@ -13,6 +14,15 @@
         {{ javascript_include('js/jquery.min.js') }}
         {{ javascript_include('js/bootstrap.min.js') }}
         {{ javascript_include('js/jquery-ui.min.js') }}
+        #}
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/jquery-ui.structure.min.css">
+        <link rel="stylesheet" href="css/jquery-ui.theme.min.css">
+        <link rel="stylesheet" href="css/estilos.css">
+
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery-ui.min.js"></script>
         <meta name="author" content="kyo">
     </head>
     <body>
