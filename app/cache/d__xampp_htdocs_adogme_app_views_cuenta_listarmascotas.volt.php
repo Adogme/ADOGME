0a4c09c02a98defa0e74998b6a0f6a0a7d6dd833
danielbellido12 <danielbellido@outlook.com>
2016-06-18 12:48:49 -0500

    Interfaces de búsqueda de perro y mis macotas
    :v:

--- app/cache/d__xampp_htdocs_adogme_app_views_cuenta_listarmascotas.volt.php
+++ app/cache/d__xampp_htdocs_adogme_app_views_cuenta_listarmascotas.volt.php
@@ -9,22 +9,43 @@
 	<?php } ?>
 </ul>
 <br>
-<div class='list-group gallery'>
-	<div class="row">
-            <div class='col-md-6'>
+<div class="row">
+	<div class='col-md-6'>
+            <div class='list-group gallery'>
                 <a class="thumbnail fancybox" rel="ligthbox" href="http://placehold.it/300x320.png">
                     <img class="img-responsive" alt="" src="http://placehold.it/320x320" />
                     <div class='text-right'>
                         <small class='text-muted'>Image Title</small>
                     </div> <!-- text-right / end -->
                 </a>
-                <?= $this->tag->linkTo(['cuenta/editarMascota', 'Editar mascota', 'class' => 'btn btn-primary btn-sm']) ?>
-            </div> <!-- col-6 / end -->
-            <div class="col-md-6">
-                Raza:<br>
-                Sexo:<br>
-                Peso:<br>
+                <?= $this->tag->linkTo(['cuenta/editarMascota', 'Editar mascota', 'class' => 'btn btn-primary']) ?>
+            </div> 
+    </div>
+    <div class="col-md-6">
+
+        <table class="table table-bordered table-striped" style="margin-top: 20px;">
+            <thead>
+                <tr>
+                    <th>Característica</th>
+                    <th>Descripción</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Raza</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Sexo</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Peso</td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table><br>
                 <button class="btn btn-primary">Dar en adopcion</button><br><br>
                 <button class="btn btn-primary">Ver adoptantes</button>
-            </div>
-     </div> <!-- row / end-->
+    </div><!-- col-6 / end -->
+</div><!-- row / end-->
